b1c0d8aa53ec4bd15d54e881fcb8ed4e5b991871
azu-ref <fjah17@gmail.com>
2019-08-06 19:34:42 -0400

    preparando entorno para exterder modelo usuario que django tienenpor defecto

--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -3,23 +3,4 @@
 #django
 from django.db import models
 
-class User(models.Model):
-    #user model
-
-    email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-
-    isAdmin = models.BooleanField(default=False)
-
-    bio = models.TextField(blank=True)
-
-    birthdate = models.DateField(blank=True, null=True)
-
-    country = models.CharField(max_length=100, blank=True, null=True)
-
-    created = models.DateTimeField(auto_now_add=True)
-    modified = models.DateTimeField(auto_now=True)
     
