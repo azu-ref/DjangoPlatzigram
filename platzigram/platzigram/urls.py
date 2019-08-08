4bb76bfda09fd8fa1f476532373cf04c3ae71e38
azu-ref <fjah17@gmail.com>
2019-08-08 01:01:40 -0400

    creando el modelo de posts y ya funcionan las imagenes

--- platzigram/platzigram/urls.py
+++ platzigram/platzigram/urls.py
@@ -1,6 +1,9 @@
 #Platzigram urls module
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from platzigram import views as local_views
 from posts import views as posts_views
 
@@ -11,4 +14,6 @@ urlpatterns = [
     path('hi/<str:name>/<int:age>', local_views.say_hi),
 
     path('posts/', posts_views.list_posts)
-]
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- platzigram/posts/models.py
+++ platzigram/posts/models.py
@@ -2,5 +2,23 @@
 
 #django
 from django.db import models
+from django.contrib.auth.models import User
 
+#models
+from users.models import Profile
+
+
+class Post(models.Model):
+    #post model
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE)
+
+    title = models.CharField(max_length=255)
+    photo = models.ImageField(upload_to='post/photos')
+
+    created = models.DateTimeField(auto_now_add=True)
+    created = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return '{} by @{}'.format(self.title, self.user.username)
     
