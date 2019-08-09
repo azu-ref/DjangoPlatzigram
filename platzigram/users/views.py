8a088fb8d0e93a0d82e65812a710c5cf35383802
azu-ref <fjah17@gmail.com>
2019-08-09 15:03:29 -0400

    creando la vista y el template para el registro de usuarios

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -1,7 +1,15 @@
 """users views"""
+#django
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+#models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+#exceptions
+from django.db.utils import IntegrityError
 
 def login_view(request):
     """login views"""
@@ -21,3 +29,30 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('login')
+
+def signup_views(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        passwd = request.POST['passwd']
+        passwd_confirmation = request.POST['passwd_confirmation']
+
+        if passwd != passwd_confirmation:
+            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
+        
+        try:
+            new_user = User.objects.create_user(username=username, password=passwd)
+
+        except IntegrityError:
+            return render(request, 'users/signup.html', {'error': 'Username is already in use'})
+
+        new_user.first_name = request.POST['first_name']
+        new_user.last_name = request.POST['last_name']
+        new_user.email = request.POST['email']
+        new_user.save()
+
+        profile = Profile(user=new_user)
+        profile.save()
+
+        return redirect('login')
+
+    return render(request, 'users/signup.html')
