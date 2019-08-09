204cbf10486fef5617074a6aad9caa687f9a81de
azu-ref <fjah17@gmail.com>
2019-08-08 22:26:57 -0400

    implementando logout

--- platzigram/users/views.py
+++ platzigram/users/views.py
@@ -1,6 +1,7 @@
 """users views"""
 from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def login_view(request):
     """login views"""
@@ -15,3 +16,8 @@ def login_view(request):
             return render(request, 'users/login.html', {'error': 'Invalid username or password'})
        
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
